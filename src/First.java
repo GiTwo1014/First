bf798b039f18cdc95f8ccd33f94b51ed3a129614
Jayle17 <710327025@qq.com>
2022-05-07 10:38:48 +0800

    hot-fix Commit1

--- src/First.java
+++ src/First.java
@@ -1,8 +1,5 @@
 public class First {
-       public static void main(String[] args) {
-           System.out.println("dasdassa");
-       }
-
-    private static void printf(String dasdas) {
-    }
+  public static void main(String[] args) {
+        System.out.println("aaaaa");
+  }
 }
